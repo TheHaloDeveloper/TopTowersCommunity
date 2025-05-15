9a5e6e784f38149f9593b91be52a712b1697939f
Halo <68120801+TheHaloDeveloper@users.noreply.github.com>
2025-05-15 16:57:57 -0700

    bg colors from difficulty

--- static/main.js
+++ static/main.js
@@ -1,6 +1,26 @@
 data = data["data"];
 
-function getColor(rank) {
+let difficulties = {
+    8: "Insane",
+    9: "Extreme",
+    10: "Terrifying",
+    11: "Catastrophic",
+    12: "Horrific",
+    13: "Unreal",
+    14: "nil"
+}
+
+let colors = {
+    "Insane": "#0000FF",
+    "Extreme": "#0389FF",
+    "Terrifying": "#00FFFF",
+    "Catastrophic": "#FFFFFF",
+    "Horrific": "#9695FF",
+    "Unreal": "#5100CB",
+    "nil": "#65666D"
+}
+
+function getColor(rank, diff) {
     if (rank == 1) {
         return "212, 188, 48";
     } else if (rank == 2) {
@@ -8,10 +28,15 @@ function getColor(rank) {
     } else if (rank == 3) {
         return "179, 125, 81";
     } else {
-        return "255, 255, 255";
+        return diff == "Catastrophic" ? "0, 0, 0" : "255, 255, 255";
     }
 }
 
+function getDifficultyName(diff) {
+    return difficulties[Math.floor(diff)]
+}
+
+let diff;
 for (let tower of data) {
     let name;
     try {
@@ -20,8 +45,12 @@ for (let tower of data) {
         name = tower[1];
     }
     
+    if (tower[3]) {
+        diff = getDifficultyName(parseFloat(tower[3]))
+    }
+    
     document.getElementById("main-list").innerHTML += `
-        <div class="list-item" style="color: rgb(${getColor(parseInt(tower[0]))});">
+        <div class="list-item" style="color: rgb(${getColor(parseInt(tower[0]), diff)}); background-color: ${colors[diff]}">
             <span class="rank">#${tower[0]}</span>&emsp;
             <span>${name}</span>
         </div>
