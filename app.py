dbf30d96f17aa39cadf84ea2d26fa3082376d4e9
Halo <68120801+TheHaloDeveloper@users.noreply.github.com>
2025-05-15 11:22:50 -0700

    google sheets setup

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
 from flask import Flask, render_template
+import os
+import requests
+from dotenv import load_dotenv
+load_dotenv()
 
 app = Flask(__name__)
+
+API_KEY = os.getenv("GOOGLE_SHEETS_API_KEY")
+SHEET_ID = "1bxhj0Xtixkpo_BtzsnIg-qwLTA7qdb7Y6fursQjRZKM"
+RANGE = "Main List!A:N"
 
 @app.route("/")
 def home():
