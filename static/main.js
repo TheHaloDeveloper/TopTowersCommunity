f9ec908f1db2156b0e9d7d54617a422451e1079a
Halo <68120801+TheHaloDeveloper@users.noreply.github.com>
2025-05-15 15:48:32 -0700

    list colors

--- static/main.js
+++ static/main.js
@@ -1,5 +1,17 @@
 data = data["data"];
 
+function getColor(rank) {
+    if (rank == 1) {
+        return "212, 188, 48";
+    } else if (rank == 2) {
+        return "136, 136, 136";
+    } else if (rank == 3) {
+        return "179, 125, 81";
+    } else {
+        return "255, 255, 255";
+    }
+}
+
 for (let tower of data) {
     let name;
     try {
@@ -9,7 +21,7 @@ for (let tower of data) {
     }
     
     document.getElementById("main-list").innerHTML += `
-        <div class="list-item">
+        <div class="list-item" style="color: rgb(${getColor(parseInt(tower[0]))});">
             <span class="rank">#${tower[0]}</span>&emsp;
             <span>${name}</span>
         </div>
