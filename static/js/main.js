e6f8d363c0cd48729c4d250bf2e8b50bf83d34ff
Halo <68120801+TheHaloDeveloper@users.noreply.github.com>
2025-08-11 21:52:10 -0700

    underscore images

--- static/js/main.js
+++ static/js/main.js
@@ -118,7 +118,7 @@ function populateList() {
                     <span class="rank">#${rank}</span>&emsp;
                     <span ${extra}>${name}</span>
                 </div>
-                <img class="list-image" src="/static/images/${name}.png"/>
+                <img class="list-image" src="/static/images/${name.replaceAll(" ", "_")}.png"/>
             </div>
         `;
     }
